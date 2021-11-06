050ed0a228d43e227171264d935e3bfe26c9db04
Rasyid <rasydridla298@gmail.com>
2021-11-06 16:43:49 +0700

    update token

--- app/src/main/java/id/rrdevfundamental/utils/UrlUtil.kt
+++ app/src/main/java/id/rrdevfundamental/utils/UrlUtil.kt
@@ -1,4 +1,4 @@
 package id.rrdevfundamental.utils
 
 const val BASE_URL = "https://api.github.com/"
-const val ACCESS_TOKEN = "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"
+const val ACCESS_TOKEN = "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"
